5a006f46858f237e34820fee0c50fcfaaed3a5e3
AVSLDOT <161859070+AVSLDOT@users.noreply.github.com>
2024-03-13 12:12:01 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,57 +9,11 @@ terraform {
 
 # Configure the AWS Provider
 
-provider "aws" {
-  region = "us-east-1"
-  sts_region = "us-east-1"
-}
-
-
-resource "aws_vpc" "jenkins-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-  Name = "jenkins"
-}
-}
-
- resource "aws_subnet" "subnet-1" {
-   vpc_id            = aws_vpc.jenkins-vpc.id
-   cidr_block        = "10.0.1.0/24"
-   map_public_ip_on_launch = true
-   depends_on = [aws_vpc.jenkins-vpc]
-   availability_zone = "us-east-1a"
-   tags = {
-     Name = "jenkins-subnet1"
-   }
- }
-
-resource "aws_route_table" "jenkins-route-table" {
-vpc_id = aws_vpc.jenkins-vpc.id
- tags = {
-     Name = "jenkins"
-   }
- }
-
-resource "aws_route_table_association" "assiciation" {
-  subnet_id      = aws_subnet.subnet-1.id
-  route_table_id = aws_route_table.jenkins-route-table.id
-}
-
-resource "aws_internet_gateway" "gw" {
- vpc_id = aws_vpc.jenkins-vpc.id
-depends_on = [aws_vpc.jenkins-vpc]
- }
-
-resource "aws_route" "jenkins-route" {
- route_table_id = aws_route_table.jenkins-route-table.id
- destination_cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.gw.id
- }
 
 resource "aws_security_group" "allow_jenkins_ssh" {
    name        = "allow_jenkins_ssh_traffic"
    description = "Allow Jenkins and ssh inbound traffic"
-     vpc_id      = aws_vpc.jenkins-vpc.id
+     vpc_id      = aws_vpc.launch-wizard-1.id
 ingress {
      description = "HTTP_Jenkins"
      from_port   = 8080
@@ -113,6 +67,5 @@ resource "aws_instance" "jenkins" {
   tags = {
     Name = "jenkins"
   }
-  security_groups = [aws_security_group.allow_jenkins_ssh.id]
-  subnet_id = aws_subnet.subnet-1.id 
+  security_groups = [aws_security_group.launch-wizard-1.id]  
 }
