d4abd22a7b58339c34e7b66ccb0abda59e77df87
AVSLDOT <161859070+AVSLDOT@users.noreply.github.com>
2024-03-13 11:09:58 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_vpc" "jenkins-vpc" {
    cidr_block        = "10.0.1.0/24"
    map_public_ip_on_launch = true
    depends_on = [aws_vpc.jenkins-vpc]
-   availabilityZone = "us-west-2a" l
+   availabilityZone = "us-west-2a"
    tags = {
      Name = "jenkins-subnet1"
    }
