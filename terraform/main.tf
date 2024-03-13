05918b4284d975790eec56c941311dcc69143f47
AVSLDOT <161859070+AVSLDOT@users.noreply.github.com>
2024-03-13 10:18:51 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,27 +33,6 @@ resource "local_file" "private_key" {
 }
 
 
-resource "aws_security_group" "security" {
-  name = "allow-all"
-
-  ingress {
-    cidr_blocks = [
-      "0.0.0.0/0"
-    ]
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-  }
-  
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}  
-
-
 resource "aws_instance" "jenkins" {
   ami           = "ami-07d9b9ddc6cd8dd30"
   instance_type = "t2.micro"
